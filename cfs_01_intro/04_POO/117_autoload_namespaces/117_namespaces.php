8eab8b9b8fdd67b8cdf680b28bc7eb6ce9caecd1
Nicole <nicolem.britto@gmail.com>
2025-11-06 07:10:57 -0300

    Autoload

--- cfs_01_intro/04_POO/117_autoload_namespaces/117_namespaces.php
+++ cfs_01_intro/04_POO/117_autoload_namespaces/117_namespaces.php
@@ -1,1 +1,20 @@
 <?php
+
+//require '../vendor/autoload.php'; -> Em vez disso, vamos usar o autoload
+//psr4 = é um padrão recomendado para carregamento automático de classes a partir de seus namespaces e caminhos de arquivo
+// = \<namespace>\<subnamespace>\<classe>
+
+require 'helpers/autoload.php';
+
+$login = new Login;
+
+//$login->email = 'email.com';
+//$login->password = '123';
+
+//echo $login->auth();
+
+$user = ['name' => 'Nicole', 'email' => 'email.com'];
+
+$userObj = (object)$user;
+
+echo $userObj->email;
